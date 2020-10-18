7d514b2dde9e9ea899a77d3580cb005793760f5b
Shahinur Islam <shahin.sec.cse@gmail.com>
2020-10-19 00:09:53 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+    // defaultLineHeights: true,
+    // standardFontWeights: true
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ff6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883'
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
